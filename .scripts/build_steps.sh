628d13b4b5ae17c48e070c48b41e9c17171c598d
nichmor <nmorkotilo@gmail.com>
2024-04-25 17:21:07 +0300

    release: v0.0.3

--- .scripts/build_steps.sh
+++ .scripts/build_steps.sh
@@ -34,13 +34,9 @@ CONDARC
 export CONDA_LIBMAMBA_SOLVER_NO_CHANNELS_FROM_INSTALLED=1
 
 mamba install --update-specs --yes --quiet --channel conda-forge --strict-channel-priority \
-    pip mamba rattler-build conda-forge-ci-setup=4 "conda-build>=24.1" rattler-build-conda-compat
+    pip mamba rattler-build conda-forge-ci-setup=4 "conda-build>=24.1"
 mamba update --update-specs --yes --quiet --channel conda-forge --strict-channel-priority \
     pip mamba rattler-build conda-forge-ci-setup=4 "conda-build>=24.1"
-  
-
-pip install git+https://github.com/nichmor/conda-forge-ci-setup-feedstock.git@feat/add-rattler-build-support-for-upload#subdirectory=recipe
-
 
 # set up the condarc
 setup_conda_rc "${FEEDSTOCK_ROOT}" "${RECIPE_ROOT}" "${CONFIG_FILE}"
